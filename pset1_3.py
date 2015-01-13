09589df4368733a59da14f6db879edc30654ba8a
lytemar@yahoo.com <lytemar@yahoo.com>
2015-01-12 22:27:05 -0600

    Update pset1_3.py

--- pset1_3.py
+++ pset1_3.py
@@ -1,3 +1,22 @@
 s = 'azcbobobegghakl'
 
-for char in s:
+start = 0
+pos = 0
+maxLen = 0
+maxStart = 0
+
+while pos < len(s):
+    strLen = 1
+    start = pos
+    while True:
+        if ord(s[pos+1]) < ord(s[pos]):
+            if strLen > maxLen:
+                maxLen = strLen
+                maxStart = start
+            pos += 1
+            break
+        else:
+            strLen += 1
+            pos += 1
+substring = s[maxStart : maxStart + strLen + 1]
+print('Longest substring in alphabetical order is: '+ substring)
